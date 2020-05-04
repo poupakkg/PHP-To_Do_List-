2e9f164a2d3364b90f21aae102085fc5413d797b
poupakkg <60771085+poupakkg@users.noreply.github.com>
2020-05-03 20:10:47 -0600

    add loop and session with method GET

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+if ( !isset( $_SESSION[ 'TO_DO_LIST' ]))
+{
+    $_SESSION[ 'TO_DO_LIST' ] = array();
+}
+
+array_push ( $_SESSION['TO_DO_LIST'], $GET['MAY_2020'] );
+
 $message = 'Welcome back Poupak to make your new list';
 ?><!DOCTYPE html>
 <html lang="en">
@@ -11,13 +19,35 @@ $message = 'Welcome back Poupak to make your new list';
 <body>
     <h1>TO DO LIST</h1>
     <?php echo $message; ?>
-    <p>
+    <form action="./index.php" method = "GET">
+
         <lable for = "TO_DO_LIST" > TO DO LIST MAY 2020 :
-        <input type = "text" name = "TO_DO_LIST" id="TO_DO_LIST" >
+        <input type = "text" name = "MAY_2020" id="MAY_2020" >
         </lable>
 
         <input type = "submit" value = "ADD TO LIST">
-    </p>
-   
+    
+    </form>
+
+      <?php if (!empty ( $_SESSION[ 'TO_DO_LIST' ]) ): ?>
+       <h2> MAY 2020:</h2>
+        <ul>
+          <?php  foreach ( $_SESSION[ 'TO_DO_LIST' ] as $MAY_2020) : ?> 
+            <li>
+                <?php echo $MAY_2020; ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endif; ?>
+      <pre>
+        <strong>$_GET contents:</strong>
+        <?php var_dump ( $_GET ); ?>
+    </pre>S
+
+      <pre>
+        <strong>$_SESSION contents:</strong>
+        <?php var_dump ( $_SESSION ); ?>
+    </pre>
+     
 </body>
 </html>
